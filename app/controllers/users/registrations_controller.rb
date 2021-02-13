abea2cf9c8e2cea417d91e4c0c1621f48c2d536a
LVQA1998 <quanganlevan@gmail.com>
2021-02-13 14:10:41 +0700

    fix unpermited params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  prepend_before_action :check_captcha, only: :create
+  # prepend_before_action :check_captcha, only: :create
   before_action :configure_sign_up_params, only: :create
   before_action :configure_update_params, only: :update
 
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password,
-      :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar,
-      :role, :birthday) }
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:name, :email, :password,:password_confirmation,
+        :remember_me, :avatar, :avatar_cache, :remove_avatar, :role, :birthday)
+    end
   end
 
   def configure_update_params
@@ -16,10 +17,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
       :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
   end
 
-  def check_captcha
-    unless verify_recaptcha
-      self.resource = resource_class.new sign_up_params
-      respond_with_navigational(resource) { render :new }
-    end
-  end
+  # def check_captcha
+  #   unless verify_recaptcha
+  #     flash[:error] = "There was an error with the recaptcha. Please do it again."
+  #     self.resource = resource_class.new sign_up_params
+  #     respond_with_navigational(resource) { render :new }
+  #   end
+  # end
 end
