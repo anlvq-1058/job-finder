6ea68be8ab299c96d9e33473f036f244c01111eb
LVQA1998 <quanganlevan@gmail.com>
2021-02-15 10:28:02 +0700

    add forgot password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: {minimum:1, maximum:50}
+  validate :avatar_size
+
+  private
+
+  def avatar_size
+    return if avatar.size < 5000000
+    
+    errors.add(:avatar, "file size must be lower than 5MB")
+  end
 
 end
