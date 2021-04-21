2368f53f5345bdae63c889ce23f56ed4bd053547
Vu Duy Tu <duytu131995@gmail.com>
2021-04-21 12:03:31 +0700

    Search job admin

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem "omniauth-facebook"
 gem "cocoon"
 gem "ckeditor"
 gem "kaminari"
-
+gem "ransack", "~> 2.4", ">= 2.4.2"
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/helpers/admin/jobs_helper.rb
+++ app/helpers/admin/jobs_helper.rb
@@ -1,2 +1,17 @@
 module Admin::JobsHelper
+  def checked_gender_job gender
+    params.dig(:q, :gender_eq) == gender ? true : false
+  end
+
+  def checked_experience_job exp
+    params.dig(:q, :experience) == exp ? true : false
+  end
+
+  def checked_salary_job salary
+    params.dig(:q, :salary) == salary ? true : false
+  end
+
+  def checked_end_at_job val
+    params.dig(:jobs_are_recruiting, :end_at) == val ? true : false
+  end
 end
